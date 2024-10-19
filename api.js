b7e5c3808c7ec7f37b35d41f638abf9c6afe24ee
Miguel <miguel.melocr@gmail.com>
2024-10-19 19:32:49 -0300

    API

---
+++ api.js
@@ -0,0 +1,29 @@
+const url = `https://api.thecatapi.com/v1/images/search?limit=20`;
+const api_key = "DEMO_API_KEY"
+
+ fetch(url,{headers: {
+      'x-api-key': api_key
+    }})
+ .then((response) => {
+   return response.json();
+ })
+.then((data) => {
+  let imagesData = data;
+  imagesData.map(function(imageData) {
+    
+    let image = document.createElement('img');
+    //use the url from the image object
+    image.src = `${imageData.url}`;
+        
+    let gridCell = document.createElement('div');
+    gridCell.classList.add('col');
+    gridCell.classList.add('col-lg');
+    gridCell.appendChild(image)
+      
+    document.getElementById('grid').appendChild(gridCell);
+    
+    });
+})
+.catch(function(error) {
+   console.log(error);
+});
